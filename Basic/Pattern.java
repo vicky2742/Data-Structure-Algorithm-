5b4cd628053014c1c68d23bc71977f1bb56ac956
Vicky <vickyjmh5@gmail.com>
2023-12-28 21:37:07 +0530

    new  update

---
+++ Basic/Pattern.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Pattern {
+    public static void main(String[] args) {
+        // System.out.println("enter the n:");
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = i; j < n; j++) {
+        //         System.out.print("*");
+
+        //     }
+        //     System.out.println();
+
+        // }
+
+            //! type conversion
+        // int a=5;
+        // long b=a;//right
+        // int c=b;//not possible
+        // System.out.println(c);
+
+        //! type casting
+
+        // float x=27.23f;
+
+        // int z=x;
+        // int y=(int)x;
+        // System.out.println(y);
+        // System.out.println(z);
+
+        char a='a';
+        short b=50;
+        System.out.println((int)a);
+        System.out.println((int)b);
+        System.out.println(a+b);
+
+    }
+}
---
+++ Basic/Week2Ques.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Week2Ques {
+    public static void main(String[] args) {
+        //ques1
+
+        Scanner sc=new Scanner(System.in);
+        // System.out.print("enter num1,num2,num3:");
+        // int num1=sc.nextInt();
+        // int num2=sc.nextInt();
+        // int num3=sc.nextInt();
+
+        // int avg=(num1+num2+num3)/3;
+        // System.out.println("average :"+avg);
+
+        //!ques2
+
+        // int side1=sc.nextInt();
+        // int side2=sc.nextInt();
+
+        // int area=side1*side2;
+        // System.out.println("area of square:"+area);
+
+        //*ques3
+
+    //     float pencil=sc.nextFloat();
+    //     float pen=sc.nextFloat();
+    //     float eraser=sc.nextFloat();
+
+    //     double totalCost=(pencil+pen+eraser);
+
+    //   System.out.println(totalCost);
+
+        // ques4
+
+        // byte a=4;
+        // char b='c';
+        // short c=513;
+        // int d=12;
+        // float e=12.2f;
+        // double f=99.999;
+
+        int $=3444;//valid
+        int v=6;//valid
+        int $vh=7;//valid
+        int _vj=77;//valid
+
+        int 4v=66;//not valid
+
+        System.out.println();
+        // double res=(f*b)+(d%a)-(b*c);
+        // System.out.println(res);
+
+        sc.close();
+    }
+}

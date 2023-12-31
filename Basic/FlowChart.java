e1b9d784e4227cb7baec8e7112f9121d6d96daad
Vicky <vickyjmh5@gmail.com>
2023-12-31 20:28:25 +0530

    new  update

--- Basic/FlowChart.java
+++ Basic/FlowChart.java
@@ -6,43 +6,43 @@ class FlowChart {
         // !ques_1 sum of two number
 
         Scanner sc = new Scanner(System.in);
-        // System.out.print("enter num1 & num2 :");
-        // int num1 = sc.nextInt();
-        // int num2 = sc.nextInt();
+        System.out.print("enter num1 & num2 :");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
 
-        // System.out.println(num1 + num2);
+        System.out.println(num1 + num2);
 
         // * ques_2 Simple Interest
 
-        // int p = sc.nextInt();
-        // int r = sc.nextInt();
-        // int t = sc.nextInt();
-        // int sI = (p * r * t) / 100;
-        // System.out.println(sI);
+        int p = sc.nextInt();
+        int r = sc.nextInt();
+        int t = sc.nextInt();
+        int sI = (p * r * t) / 100;
+        System.out.println(sI);
 
         // ? ques_3 max of 3 number
 
-        // System.out.print("enter a , b & c :");
-        // int a = sc.nextInt();
-        // int b = sc.nextInt();
-        // int c = sc.nextInt();
+        System.out.print("enter a , b & c :");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
 
-        // if (a > b && a > c) {
-        // System.out.println("a");
-        // }
+        if (a > b && a > c) {
+        System.out.println("a");
+        }
 
-        // else if (b > a && b > c) {
-        // System.out.println("b");
-        // }
+        else if (b > a && b > c) {
+        System.out.println("b");
+        }
 
-        // else {
-        // System.out.println("c");
-        // }
+        else {
+        System.out.println("c");
+        }
 
         // TODO: ques3 prime number
 
-        // System.out.print("number :");
-        // int n = sc.nextInt();
+        System.out.print("number :");
+        int n = sc.nextInt();
 
         // ! sum of n natural number
 

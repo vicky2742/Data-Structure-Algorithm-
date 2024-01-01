1234169e18261d2389875a3aa2cc0d452d6fe6b0
Vicky <vickyjmh5@gmail.com>
2024-01-01 22:13:51 +0530

    new  update

---
+++ Basic/ConditionalStatement.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+public class ConditionalStatement {
+    public static void main(String[] args) {
+        // largest number
+
+        Scanner sc = new Scanner(System.in);
+        // System.out.print("enter the num:");
+        // int num1=sc.nextInt();
+        // int num2=sc.nextInt();
+
+        // if(num1<num2){
+        // System.out.println("num2 is larger,");
+        // }
+        // else{
+        // System.out.println("num1 is larger");
+        // }
+
+        // !even & odd
+        // System.out.print("enter the number :");
+        // int num = sc.nextInt();
+
+        // if (num % 2 == 0) {
+        // System.out.println(num + " is even");
+        // }
+
+        // else {
+        // System.out.println(num + " is odd");
+        // }
+
+        // *income tax
+        // int income = sc.nextInt();
+        // int tax;
+        // if (income < 500000) {
+        // tax = 0;
+        // } else if (income > 500000 && income < 100000) {
+        // tax = (int) (income * 0.2);
+        // } else {
+        // tax = (int) (income * 0.3);
+        // }
+        // System.out.println(tax);
+
+        // ?calculator
+        System.out.println("------------------------------");
+        System.out.print("num1 , num2 :");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println("Choose Operator: +,-,x,/");
+        char operator = sc.next().charAt(0);
+
+        switch (operator) {
+            case '+':
+                System.out.println(num1 + num2);
+                break;
+            case '-':
+                System.out.println(num1 - num2);
+                break;
+            case '*':
+                System.out.println(num1 * num2);
+                break;
+            case '/':
+                System.out.println(num1 / num2);
+                break;
+
+            default:
+                System.err.println("Wrong Operator");
+                break;
+        }
+        System.out.println("------------------------------");
+
+        sc.close();
+    }
+}

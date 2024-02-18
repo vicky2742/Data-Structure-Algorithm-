4c5bb92e02d0d6c02e980e1ca4987aeb01cad1ac
Vicky <vickyjmh5@gmail.com>
2024-02-18 22:20:02 +0530

    new  update

---
+++ BitwiseOperator/Op.java
@@ -0,0 +1,14 @@
+
+public class Op {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        System.out.println("a & b : " + (a & b));
+        System.out.println("a | b : " + (a | b));
+        System.out.println("a ^ b : " + (a ^ b));
+        System.out.println("~a :" + ~a);
+        System.out.println("a << b :" + (a << 2));
+        System.out.println("a >> b :" + (a >> 2));
+
+    }
+}

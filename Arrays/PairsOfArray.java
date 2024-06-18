8189cdd79966b34958ec43db3d8cf1f03a5ee931
Vicky <vickyjmh5@gmail.com>
2024-06-18 12:45:11 +0530

    first commit

--- Arrays/PairsOfArray.java
+++ Arrays/PairsOfArray.java
@@ -1,8 +1,16 @@
 public class PairsOfArray {
+    static void displayArr(int arr[]) {
+        System.out.print("Array : ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
     public static void main(String[] args) {
         int arr[] = { 2, 4, 6, 8, 10 };
+        displayArr(arr);
         int totalPair = 0;
-
+        System.out.println();
         for (int i = 0; i < arr.length; i++) {
             int currentElement = arr[i];
 
@@ -12,7 +20,8 @@ public class PairsOfArray {
             }
             System.out.println();
         }
-        System.out.println("Total Pairs : "+totalPair);
+
+        System.out.println("Total Pairs : " + totalPair);
 
     }
 }
--- Basic/Format.java
+++ Basic/Format.java
@@ -1,9 +1,11 @@
-import java.text.DecimalFormat;;
+import java.text.DecimalFormat;
 
 public class Format {
     public static void main(String[] args) {
         double d = 1232.22222;
         DecimalFormat df = new DecimalFormat("##.##");
+        System.out.format("%.3f", d);
+        System.out.println();
         System.out.println(df.format(d));
 
     }
